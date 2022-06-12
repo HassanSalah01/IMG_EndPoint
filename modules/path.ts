7255db36514ca9c4d8f0e47ecacaeb524da9521c
HassanSalah01 <hassan.salah.eldine1@gmail.com>
2022-06-12 21:35:24 +0200

    Image_Processing_Modules_Path

---
+++ modules/path.ts
@@ -0,0 +1,4 @@
+import path from 'path';
+export const imageSource = path.resolve(__dirname, '../images');
+export const thumbnail = path.resolve(__dirname, '../thumbnail');
+export const direName = path.resolve(__dirname, '../../');
