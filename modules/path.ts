876922533e2c51ce3e02e4370be56e2e07187433
HassanSalah01 <hassan.salah.eldine1@gmail.com>
2022-09-06 21:21:30 +0200

    updated_files

--- modules/path.ts
+++
@@ -1,4 +0,0 @@
-import path from 'path';
-export const imageSource = path.resolve(__dirname, '../images');
-export const thumbnail = path.resolve(__dirname, '../thumbnail');
-export const direName = path.resolve(__dirname, '../../');
